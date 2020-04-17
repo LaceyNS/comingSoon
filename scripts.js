2bf5ec9e7db11e74273e0f9f2e3832e3a8da4553
Lacey Savage <lacey.savage@gmail.com>
2020-04-16 21:16:16 -0400

    Add lighter shading & typed js

---
+++ scripts.js
@@ -0,0 +1,12 @@
+//Document ready
+$(function() {
+    const typed = new Typed(".typed", {
+        strings: ["under construction...", "a work in progress...", "coming soon..."],
+        stringsElement: null,
+        typeSpeed: 60,
+        startDelay: 500,
+        backSpeed: 40,
+        backDelay: 800,
+        loop: true
+    });
+});
